7f6fd714a9da1a9e1b0cffc47d24ab810538a18a
kunal162aiims <kunal162@gmail.com>
2026-09-13 14:27:52 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="arjun-world-v6";
+const CACHE="arjun-world-v7";
 const STATIC=["./manifest.webmanifest","./world_reference_map.png","./icon-512.png","./apple-touch-icon.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
